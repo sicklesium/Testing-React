39565a548cefe884d2e82125c8703565ea8d32ce
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-02-16 20:08:40 -0500

    Third test runs as expected.

--- star-wars-app/src/App.test.js
+++ star-wars-app/src/App.test.js
@@ -4,7 +4,7 @@ import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 import axios from "axios";
 
-test("renders with no problems", async () => {
+test("App renders", async () => {
     const wrapper = rtl.render(<App />);
 
     const logo = wrapper.getByAltText(/logo/i);
@@ -22,10 +22,21 @@ jest.mock("axios", () => {
     }
 })
 
-test('api call successful', async () => {
+test("API successfully is called", async () => {
     const wrapper = rtl.render(<App />);
 
     await wrapper.findAllByAltText(/logo/i);
 
     expect(axios.get).toHaveBeenCalled();
+})
+
+test("Next Button calls upon API", async () => {
+
+    const wrapper = rtl.render(<App />);
+    const next = wrapper.getByText(/next/i)
+    rtl.act(() => {
+        rtl.fireEvent.click(next)
+    })
+
+    expect(axios.get).toHaveBeenCalled()
 })
