6bb5e5211e956890cefbdca5555bc4649591cb75
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-02-16 22:18:07 -0500

    mvp reached, added one more test

--- star-wars-app/src/App.test.js
+++ star-wars-app/src/App.test.js
@@ -4,7 +4,7 @@ import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 import axios from "axios";
 
-test("App renders", async () => {
+test("App renders", () => {
     const wrapper = rtl.render(<App />);
 
     const logo = wrapper.getByAltText(/logo/i);
@@ -16,27 +16,30 @@ jest.mock("axios", () => {
     return {
         get: jest.fn(() => Promise.resolve({
             data: {
-                results: ['Simon', '169', '69', 'red', 'red']
+                results: [{ name: 'Simon', url: 'asdjgjashgta' }]
             }
         }))
     }
 })
 
-test("API successfully is called", async () => {
+test("API successfully is called", () => {
     const wrapper = rtl.render(<App />);
 
-    await wrapper.findAllByAltText(/logo/i);
+    wrapper.getByAltText(/logo/i);
 
     expect(axios.get).toHaveBeenCalled();
 })
 
-test("Next Button calls upon API", async () => {
+test("Next Button calls upon API", () => {
 
     const wrapper = rtl.render(<App />);
     const next = wrapper.getByText(/next/i)
-    rtl.act(() => {
-        rtl.fireEvent.click(next)
-    })
+    rtl.fireEvent.click(next)
 
     expect(axios.get).toHaveBeenCalled()
+})
+
+test('renders result of API call', async () => {
+    const { findByText } = rtl.render(<App />)
+    expect(await findByText(/simon/i)).toBeVisible()
 })
